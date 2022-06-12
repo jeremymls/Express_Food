79c8e43d37b476776ff1207ec1cbd3182fc1045d
jeremy-mls <contact@jeremy_monlouis.fr>
2022-06-12 13:21:10 +0200

    version soutenance

--- js/chef.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("id_produit").addEventListener("change", function (e) {
-  let id = e.target.value;
-  let url = "chef.php?" + (id != 0 ? "id_produit=" + id : "");
-  window.location.href = url;
-});
