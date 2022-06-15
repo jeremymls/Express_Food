0072f38edeca4f905da369799fac1b4dfd4a7c27
jeremy-mls <contact@jeremy_monlouis.fr>
2022-06-15 19:07:43 +0200

    gitignore

--- Demo/constants.php
+++ Demo/constants.php
@@ -1,11 +1,6 @@
 <?php
 // Accès BDD MySQL
 const HOST = 'localhost';
-const USER = 'root';
-const PASS = 'root';
+const USER = '******';
+const PASS = '******';
 const DB = 'expressfood';
-
-// Accès BAL Gmail
-const MB_HOST = '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX';
-const MB_USER = 'greta.oise2@gmail.com';
-const MB_PASS = 'Gret@2020';
